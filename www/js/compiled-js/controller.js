3b93c8a447bb65c3f7a51c4c3db72316bbd13a2d
writeosahon <wrwiteosahon@yahoo.co.uk>
2018-10-07 17:34:29 +0100

    initial commit

--- www/js/compiled-js/controller.js
+++ www/js/compiled-js/controller.js
@@ -117,9 +117,8 @@ utopiasoftware[utopiasoftware_app_namespace].controller = {
                     dropzone: $('#sample-puzzle-page .puzzle-drop-zone').get()
                 }).on("drag:start", function(dragStartEvent){
                     $(dragStartEvent.source).removeClass("animated shake tada");
-                    console.log("ORGINAL SOURCE", dragStartEvent.originalSource);
-                    console.log("ORGINAL CONTAINER", dragStartEvent.sourceContainer);
-                });
+                }).on("droppable:dropped", function(dragStartEvent){
+                    $(dragStartEvent.source).addClass("animated tada");
 
 
                 $('#loader-modal').get(0).hide(); // hide loader
